699105fe8a7760ef9d8f390bdc4767234a15f43a
Silvio Huebner-Schurch <silvio@vicinitycommerce.com>
2016-01-11 00:47:46 -0800

    responsive design and style edits

--- script.js
+++ script.js
@@ -25,13 +25,13 @@
     };
 
     self.updatedom = function(){
-      var thishtml='';
+      var thishtml="<div class='row'>";
 
       $.each(self.products, function(i, product){
-        if (i % 3 === 0 ) thishtml += "<div class='row'>";
         thishtml += self.products[i].htmlview;
-        if ((i % 3 == 2) || i == (self.products.length-1)) thishtml += "</div>";
       });
+
+      thishtml += "</div>";
 
       $("#content").append(thishtml);
     };
@@ -46,7 +46,7 @@
     self.url          = product.url;
     self.htmlview     = "";
     self.index        = i;
-    self.custom_class = "col" + ((i % 3) +1);
+    self.custom_class = "col-md-4";
 
     self.updatehtml = function(template){
       self.htmlview = template.replace('{image}', self.photo).replace('{title}', self.title).replace('{tagline}', self.tagline).replace('{url}', self.url).replace('{custom_class}', self.custom_class);
